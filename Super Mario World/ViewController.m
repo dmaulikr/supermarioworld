69bafc5ae389328175546fdb43b10fd84ea7378f
jonolane <jonathan.lane@pop.belmont.edu>
2016-04-27 11:47:39 -0500

    Motion set and actions recieving

--- Super Mario World/ViewController.m
+++ Super Mario World/ViewController.m
@@ -15,6 +15,7 @@
 @property (weak, nonatomic) IBOutlet UIView *containerView;
 @property (weak, nonatomic) IBOutlet UILabel *instructionsView;
 @property (strong, nonatomic) SocketIO *socketIO;
+@property (strong, nonatomic) CMMotionManager *motionSensor;
 
 @end
 
@@ -25,11 +26,12 @@
     // Do any additional setup after loading the view, typically from a nib.
     [self importSocket];
     [self animateInstructions];
+    [self startMotionDetection];
 }
 
 - (void)importSocket {
     self.socketIO = [[SocketIO alloc] initWithDelegate:self];
-    [self.socketIO connectToHost:@"localhost" onPort:4000];
+    [self.socketIO connectToHost:@"10.0.1.13" onPort:4000];
 }
 
 - (void)animateInstructions {
@@ -43,6 +45,57 @@
                      } completion:nil];
 }
 
+-(CMMotionManager *)motionSensor {
+    if (!_motionSensor) {
+        _motionSensor = [[CMMotionManager alloc] init];
+    }
+    return _motionSensor;
+}
+
+- (void)startMotionDetection {
+//    ViewController * __weak weakSelf = self;
+    if (self.motionSensor.accelerometerAvailable) {
+        self.motionSensor.accelerometerUpdateInterval = 1.0f/60.0f;
+        [self.motionSensor startAccelerometerUpdatesToQueue:[NSOperationQueue mainQueue] withHandler:^(CMAccelerometerData *data, NSError *error) {
+        
+            [self handleAccelerometerData:data];
+        }];
+    }
+}
+
+- (void)handleAccelerometerData:(CMAccelerometerData *)data {
+    
+    NSInteger yValue = roundf(data.acceleration.y * 100.0f);
+    NSInteger xValue = roundf(data.acceleration.x * 100.0f) - 48.0f;
+    
+    NSLog(@"x: %li, y: %li", (long)xValue, (long)yValue);
+
+    if(labs(yValue) > 10) {
+        
+        if(yValue > 0) {
+            
+            [self marioRight];
+        }
+        else {
+            
+            [self marioLeft];
+        }
+    }
+    else if(labs(xValue) > 20) {
+        
+        if(xValue > 0) {
+            
+            [self marioDown];
+        }
+        else {
+            
+            [self marioUp];
+        }
+    }
+}
+
+#pragma mark - Mario control
+
 - (IBAction)marioAction:(id)sender {
 
     [self.socketIO sendEvent:@"actions" withData:@"action"];
@@ -53,27 +106,47 @@
     [self.socketIO sendEvent:@"actions" withData:@"jump"];
 }
 
-#pragma mark - SocketIODelegate
-
-- (void) socketIODidConnect:(SocketIO *)socket {
-
-    NSLog(@"Did Connect");
+- (void)marioUp {
     
+    [self.socketIO sendEvent:@"actions" withData:@"up"];
 }
 
-- (void) socketIODidDisconnect:(SocketIO *)socket disconnectedWithError:(NSError *)error {
+- (void)marioDown {
     
-    NSLog(@"Did Discconnect: %@", error.localizedDescription);
+    [self.socketIO sendEvent:@"actions" withData:@"down"];
 }
 
-- (void) socketIO:(SocketIO *)socket didSendMessage:(SocketIOPacket *)packet {
+- (void)marioLeft {
     
-    NSLog(@"Did Send Message");
+    [self.socketIO sendEvent:@"actions" withData:@"left"];
 }
 
-- (void) socketIO:(SocketIO *)socket onError:(NSError *)error {
+- (void)marioRight {
     
-    NSLog(@"Did Error: %@", error.localizedDescription);
+    [self.socketIO sendEvent:@"actions" withData:@"right"];
 }
+
+//#pragma mark - SocketIODelegate
+//
+//- (void) socketIODidConnect:(SocketIO *)socket {
+//
+//    NSLog(@"Did Connect");
+//    
+//}
+//
+//- (void) socketIODidDisconnect:(SocketIO *)socket disconnectedWithError:(NSError *)error {
+//    
+//    NSLog(@"Did Discconnect: %@", error.localizedDescription);
+//}
+//
+//- (void) socketIO:(SocketIO *)socket didSendMessage:(SocketIOPacket *)packet {
+//    
+//    NSLog(@"Did Send Message");
+//}
+//
+//- (void) socketIO:(SocketIO *)socket onError:(NSError *)error {
+//    
+//    NSLog(@"Did Error: %@", error.localizedDescription);
+//}
 
 @end
